5bdaa890c8cbbed61c6133c13b73b449cc7d0cd8
JosueTorres2108 <josuett2108@gmail.com>
2024-04-04 18:55:32 -0500

    Actualizacion

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/flask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/flaskjosue'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
